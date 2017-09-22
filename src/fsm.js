e95dd1f864913848e197b154c9972776f57756ab
Goncharuk Yaroslav <gonyarik@gmail.com>
2017-09-22 22:48:49 +0300

    25 passing

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,81 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+      this.initial = [config.initial];
+      this.head = 0;
+      this.states = {
+          normal: {
+              transitions: {
+                  study: config.states.normal.transitions.study
+              }
+          },
+          busy: {
+              transitions: {
+                  get_tired: config.states.busy.transitions.get_tired,
+                  get_hungry: config.states.busy.transitions.get_hungry,
+              }
+          },
+          hungry: {
+              transitions: {
+                  eat: config.states.hungry.transitions.eat
+              }
+          },
+          sleeping: {
+              transitions: {
+                  get_hungry: config.states.sleeping.transitions.get_hungry,
+                  get_up: config.states.sleeping.transitions.get_up,
+              }
+          }
+      }
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+      return this.initial[this.head];
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+      if (!(state in this.states)) {
+        throw new SyntaxError("The state is missing!");
+      } else {
+        this.initial.length = this.head + 1; //Cut all states after head.
+        this.initial.push(state);
+        this.head++;
+      }
+
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+      if (!(event in this.states[this.initial[this.head]].transitions)) {
+        throw new SyntaxError("The transition is missing!");
+      } else {
+        this.initial.length = this.head + 1; //Cut all states after head.
+        this.initial.push(this.states[this.initial[this.head]].transitions[event]);
+        this.head++;
+      }
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+      this.initial.length = 0;
+      this.initial.push('normal');
+      this.head = 0;
+
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +85,56 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+      if (event === undefined) {
+        return Object.keys(this.states);
+      }
+      let events = [];
+      for (let state in this.states) {
+        if (event in this.states[state].transitions) {
+          events.push(state);
+        }
+      }
+      return events;
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+      if (this.head > 0) {
+      this.head--;
+      return true;
+      } else {
+          return false;
+        }
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+      if (this.initial.length !== this.head + 1) {
+      this.head++;
+      return true;
+      } else {
+          return false;
+        }
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+      let currentState = this.initial[this.head];
+      this.initial.length = 0;
+      this.head = 0;
+      this.initial.push(currentState);
+    }
 }
 
 module.exports = FSM;
